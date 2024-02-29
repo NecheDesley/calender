0f32da897ee1b55a8fa3cddd077c785c51af875f
Samuel Chinecherem <samdesley@gmail.com>
2024-02-29 18:23:25 +0000

    first commit

---
+++ calender.py
@@ -0,0 +1,9 @@
+import calender 
+#imported the calender module
+cal = calender.Calender()
+year = 2023
+month = 9
+y = calender.month(year, month)
+print(y)
+
+print('Hence,' + ' your birthday is on sunday!!')
